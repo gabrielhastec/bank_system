b1ac01ca4c0ae489c397dc29de8b179dd9f46840
Gabriel Hastec <gabrielhastec.dev@gmail.com>
2025-08-22 11:55:49 -0300

    chore: adicionar __init__.py para transformar pastas em pacotes Python

---
+++ bank/__init__.py
@@ -0,0 +1,5 @@
+# bank/__init__.py
+"""
+Pacote principal do sistema bancário.
+Contém módulos relacionados a contas, transações e autenticação.
+"""
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+# tests/__init__.py
+"""
+Pacote principal do sistema bancário.
+Contém módulos relacionados a contas, transações e autenticação.
+"""
