10814fe1a734348a87460d3e44aeac7818ef629f
gabrielhastec <gabrielhastec.dev@gmail.com>
2025-12-01 23:41:04 -0300

    Refactor: Mudança em toda a arquitetura do projeto para arquitetura DDD, possibilitando escalabilidade, segurança e adicionando abertura para integrações.

--- bank/__init__.py
+++
@@ -1,5 +0,0 @@
-# bank/__init__.py
-"""
-Pacote principal do sistema bancário.
-Contém módulos relacionados a contas, transações e autenticação.
-"""
--- bank/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Módulo principal do Core.
-
-Responsável por inicializar componentes centrais do sistema,
-como conexão com o banco, configuração e logging.
-"""
