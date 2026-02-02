ca17b3af5a5fc34e3676a44805162a17125610ca
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-02-02 20:18:23 -0300

    Feat(config): Atualiza as configurações globais e o container da aplicação.

--- backend/src/config/container.py
+++ backend/src/config/container.py
@@ -27,9 +27,13 @@ from ..application.use_cases.make_deposit import MakeDepositUseCase
 from ..application.use_cases.make_withdrawal import MakeWithdrawalUseCase
 from ..application.use_cases.make_transfer import MakeTransferUseCase
 from ..application.use_cases.get_statement import GetStatementUseCase
+
 from ..infrastructure.repositories.account_repo_sqlite import AccountRepositorySQLite
 from ..infrastructure.repositories.customer_repo_sqlite import CustomerRepositorySQLite
+from ..infrastructure.repositories.transaction_repo_sqlite import TransactionRepositorySQLite
+
 from ..infrastructure.services.notification_service import ConsoleNotificationService
+from ..infrastructure.services.jwt_service import JWTService
 
 class Container(containers.DeclarativeContainer):
     """
@@ -42,9 +46,11 @@ class Container(containers.DeclarativeContainer):
     # Repositórios (Infraestrutura)
     account_repo = providers.Singleton(AccountRepositorySQLite)
     customer_repo = providers.Singleton(CustomerRepositorySQLite)
+    transaction_repo = providers.Singleton(TransactionRepositorySQLite)
 
     # Serviços externos (Infraestrutura)
     notifier = providers.Singleton(ConsoleNotificationService)
+    jwt_service = providers.Singleton(JWTService)
 
     # Casos de uso (Aplicação)
     open_account_uc = providers.Factory(
@@ -63,21 +69,25 @@ class Container(containers.DeclarativeContainer):
     deposit_uc = providers.Factory(
         MakeDepositUseCase,
         account_repo=account_repo,
+        transaction_repo=transaction_repo,
     )
 
     withdrawal_uc = providers.Factory(
         MakeWithdrawalUseCase,
         account_repo=account_repo,
+        transaction_repo=transaction_repo,
     )
 
     transfer_uc = providers.Factory(
         MakeTransferUseCase,
         account_repo=account_repo,
+        transaction_repo=transaction_repo,
     )
 
     statement_uc = providers.Factory(
         GetStatementUseCase,
         account_repo=account_repo,
+        transaction_repo=transaction_repo,
     )
 
 # Instância global do contêiner
